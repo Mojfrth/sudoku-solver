1a102f2e5116864705bb5134f87d949af249f49a
mohit-2756 <113184967+mohit-2756@users.noreply.github.com>
2023-05-16 22:24:23 +0530

    code added

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import { Sudoku } from "./Sudoku.js"
+import { Util } from "./Util.js";
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, './sudoku-solver/build')))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './sudoku-solver/build/index.html'))
+})
+
+app.listen(5000, () => {
+    console.log("Server Running at port 5000");
+});
+
+app.get("/puzzle", (req, res) => {
+    let sudoku = new Sudoku();
+    let puzzle = sudoku.puzzle;
+    res.status(200).send({ game: puzzle });
+});
+
+app.post("/solve", (req, res) => {
+    let puzzle = [];
+    // Util.print2DArray(puzzle);
+    Util.copyGrid(req.body.board, puzzle);
+    let sudoku = new Sudoku(puzzle);
+    let solution = sudoku.isSolvable();
+    let solvedSudoku;
+    let status;
+    if (solution) {
+        solvedSudoku = sudoku.solvedPuzzle;
+        // Util.print2DArray(solvedSudoku);
+        status = true;
+    }
+    else {
+        solvedSudoku = req.body.board;
+        status = false;
+    }
+    res.status(200).send({ solution: solvedSudoku, status: status });
+});
+
+
+app.post("/validate", (req, res) => {
+    let puzzle = [];
+    Util.copyGrid(req.body.board, puzzle);
+    let sudoku = new Sudoku(puzzle);
+    let status = sudoku.validate();
+    res.status(200).send({ status: status });
+})
